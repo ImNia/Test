b6ca43fcc99f5c43694c36667b2f6046ad7f8250
ImNia <agnia-kimi1996@mail.ru>
2024-03-16 20:29:23 +0700

    change text

--- app/src/main/java/com/example/test/MainActivity.kt
+++ app/src/main/java/com/example/test/MainActivity.kt
@@ -38,6 +38,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     TestTheme {
-        Greeting("Android")
+        Greeting("I'm trying")
     }
 }
